b25b964c5ba0002926049f40542719e906f17871
choiseongho <chltjdgh3@naver.com>
2024-03-10 20:13:36 +0900

    Feat: user Store 및 type 예시 추가

--- frontend/mancity-project/src/stores/userStore.ts
+++ frontend/mancity-project/src/stores/userStore.ts
@@ -1,3 +1,22 @@
-export const userStore = () => {
-  return;
-};
+import create from "zustand";
+import { userType } from "../types/userType";
+
+const useUserStore = create<userType>((set) => ({
+  userData: {
+    userNo: 0,
+    email: "",
+    password: "",
+    nickname: "",
+    birth: 0,
+    gender: 0,
+    stature: 0,
+    weight: 0,
+    mainFoot: "",
+  },
+  setUser: (newData: Partial<userType>) =>
+    set((prev) => ({
+      userData: { ...prev.userData, ...newData },
+    })),
+}));
+
+export default useUserStore;
---
+++ frontend/mancity-project/src/types/userType.ts
@@ -0,0 +1,13 @@
+export type userType = {
+  userData: {
+    userNo: number;
+    email: string;
+    password: string;
+    nickname: string;
+    birth: number;
+    gender: number;
+    stature: number;
+    weight: number;
+    mainFoot: string;
+  };
+};
