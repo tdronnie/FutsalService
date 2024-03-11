7a6505ae184070a0b3629ea7992f55658e7dadb6
예준성 <306yyy@naver.com>
2024-03-11 18:59:29 +0900

    Chore : Eureka 서버 초기 세팅

---
+++ eureka/src/test/java/com/mancity/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mancity.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
