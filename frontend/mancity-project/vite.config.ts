069e64fc4a8d44ebb63194e90e349fc5630a899a
Jiyong <aop8021@naver.com>
2024-03-12 22:06:44 +0900

    Chore: shadcn 설정 완료

--- frontend/mancity-project/vite.config.ts
+++ frontend/mancity-project/vite.config.ts
@@ -1,8 +1,15 @@
+import path from "path"
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
-import react from "@vitejs/plugin-react";
 
 // https://vite-pwa-org.netlify.app/guide/
 export default defineConfig({
   plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  // shadcn/ui
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
