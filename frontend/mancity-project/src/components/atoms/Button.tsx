f36d979d429c4ad2cf6b5c057991ed9c3d5baf74
choiseongho <chltjdgh3@naver.com>
2024-03-12 23:49:40 +0900

    Feat: Button atom 수정

--- frontend/mancity-project/src/components/atoms/Button.tsx
+++ frontend/mancity-project/src/components/atoms/Button.tsx
@@ -1,35 +1,16 @@
 import "./button.css";
 
 interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
   primary?: boolean;
-  /**
-   * What background color to use
-   */
   backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
   size?: "small" | "medium" | "large";
-  /**
-   * Button contents
-   */
   label: string;
-  /**
-   * Optional click handler
-   */
   onClick?: () => void;
 }
 
-/**
- * Primary UI component for user interaction
- */
 export function Button({
   primary = false,
   size = "medium",
-
   backgroundColor,
   label,
   ...props
