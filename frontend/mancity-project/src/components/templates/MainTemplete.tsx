ae54fb04aec189724c3d66eac1cc0e97a8a783f6
choiseongho <chltjdgh3@naver.com>
2024-03-13 18:06:13 +0900

    Rename: 폴더 구조 변경

--- frontend/mancity-project/src/components/templates/MainTemplete.tsx
+++ frontend/mancity-project/src/components/templates/MainTemplete.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const MainTemplete = () => {
-  return (
-    <div>MainTemplete</div>
-  )
-}
+  return <div>MainTemplete</div>;
+};
 
-export default MainTemplete
+export default MainTemplete;
--- frontend/mancity-project/src/routes/index.tsx
+++ frontend/mancity-project/src/routes/index.tsx
@@ -1,14 +1,14 @@
 // src/routes/index.tsx
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import HomePage from "../components/pages/HomePage";
-import SignUpPage from "../components/pages/SignUpPage";
-import NotFoundPage from "../components/pages/NotFoundPage";
+import HomePage from "../pages/HomePage";
+import SignUpPage from "../pages/SignUpPage";
+import NotFoundPage from "../pages/NotFoundPage";
 
 const AppRoutes = () => (
   <Routes>
     <Route path="/" element={<HomePage />} />
-    <Route path="/SignUp" element={<SignUpPage />} />
+    <Route path="/signup" element={<SignUpPage />} />
     <Route path="*" element={<NotFoundPage />} />
   </Routes>
 );
