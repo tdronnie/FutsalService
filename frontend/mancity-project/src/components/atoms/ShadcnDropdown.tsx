d17f8689c5f03aacd2109e036c6a92bf84b9166a
choiseongho <chltjdgh3@naver.com>
2024-03-13 22:47:22 +0900

    Feat: custom dropdown atom 생성

--- frontend/mancity-project/src/components/atoms/ShadcnDropdown.tsx
+++ frontend/mancity-project/src/components/atoms/ShadcnDropdown.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 import {
   DropdownMenu,
@@ -11,16 +11,28 @@ import {
 } from "@/components/ui/dropdown-menu";
 
 interface PropsType {
-  items: { value: string; label: string }[];
+  items: { value: number; label: string }[];
   width?: string;
 }
 
 const ShadcnDropdown = ({ items, width = "w-40" }: PropsType) => {
   const [position, setPosition] = useState("click");
+  const [numberValue, setNumberValue] = useState(0);
 
-  useEffect(() => {
-    console.log(position);
-  });
+  // useEffect(() => {
+  //   console.log(position);
+  //   console.log(numberValue);
+  //   console.log(typeof numberValue);
+  // });
+
+  // 선택된 label을 기반으로 items 배열에서 해당 value 찾기
+  const handleValueChange = (selectedLabel: string) => {
+    const selectedItem = items.find((item) => item.label === selectedLabel);
+    if (selectedItem) {
+      setPosition(selectedLabel);
+      setNumberValue(selectedItem.value);
+    }
+  };
 
   return (
     <DropdownMenu>
@@ -29,13 +41,16 @@ const ShadcnDropdown = ({ items, width = "w-40" }: PropsType) => {
           className={`${width} h-7 border-b-2 border-[#2C4A60] flex justify-between`}
         >
           <p className=" ml-2 ">{position}</p>
-          <p className=" mr-1 ">⋎</p>
+          <p className=" mr-1 ">▼</p>
         </div>
       </DropdownMenuTrigger>
       <DropdownMenuContent className={width}>
         {/* <DropdownMenuLabel>Panel Position</DropdownMenuLabel> */}
         {/* <DropdownMenuSeparator /> */}
-        <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
+        <DropdownMenuRadioGroup
+          value={position}
+          onValueChange={handleValueChange}
+        >
           {items.map((item) => (
             <DropdownMenuRadioItem key={item.value} value={item.label}>
               {item.label}
