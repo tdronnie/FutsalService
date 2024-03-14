7759174015cc5b645e311c3abde464919d5513e2
choiseongho <chltjdgh3@naver.com>
2024-03-14 13:33:10 +0900

    Feat: Button story 생성

---
+++ frontend/mancity-project/src/components/atoms/global_button/GlabelButton.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import GlobalButton from "./GlobalButton";
+
+const meta: Meta<typeof GlobalButton> = {
+  title: "Components/atoms/Button/GlobalButton",
+  tags: ["autodocs"],
+  component: GlobalButton,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof GlobalButton>;
+
+export const Default: Story = {
+  args: {
+    width: "w-80",
+    label: "placeholder",
+  },
+};
+
+export const Half: Story = {
+  args: {
+    width: "w-40",
+    label: "placeholder",
+  },
+};
