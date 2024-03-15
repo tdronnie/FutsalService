9af909bc0ef645994e37f3dc415caa4f33a0faa0
Jiyong <aop8021@naver.com>
2024-03-15 15:01:17 +0900

    Feat: 라우팅 설정 완료

--- frontend/mancity-project/src/pages/analysis/MatchBoardPage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const MatchBoardPage = () => {
-  return (
-    <div>MatchBoardPage</div>
-  )
-}
-
-export default MatchBoardPage
--- frontend/mancity-project/src/pages/analysis/TotalFeedbackPage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const TotalFeedbackPage = () => {
-  return (
-    <div>TotalFeedbackPage</div>
-  )
-}
-
-export default TotalFeedbackPage
