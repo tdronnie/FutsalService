34a7eaed20dc86bcfd0851ba1b6228823313e967
choiseongho <chltjdgh3@naver.com>
2024-03-15 13:40:03 +0900

    Fix: ClubButton atom prop 수정

--- frontend/mancity-project/src/components/atoms/club_button/ClubButton.tsx
+++ frontend/mancity-project/src/components/atoms/club_button/ClubButton.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 
 const ClubButton = (props: TailwindPropsType) => {
-  const { textColor, bgColor, label } = props;
+  const {
+    textColor,
+    bgColor,
+    borderColor,
+    hoverTextColor,
+    hoverBgColor,
+    label,
+  } = props;
   return (
     <button
       type="button"
-      className={` text-${textColor} bg-${bgColor} font-medium text-[0.7rem] w-20 h-7 rounded-full
-      hover:text-${bgColor} hover:bg-${textColor} border-${textColor} border-2`}
+      className={` ${textColor} ${bgColor} ${borderColor}  hover:${hoverTextColor} hover:${hoverBgColor} 
+      font-medium text-[0.7rem] w-20 h-7 rounded-full border-2`}
     >
       {label}
     </button>
