6bfd1491145c6a098e0d121c5d16bca9a18231a0
Jiyong <aop8021@naver.com>
2024-03-16 23:40:38 +0900

    Feat: navbar 라우팅 설정

--- frontend/mancity-project/src/routes/index.tsx
+++ frontend/mancity-project/src/routes/index.tsx
@@ -13,8 +13,7 @@ import MatchRegisterPage from "@/pages/match/MatchRegisterPage";
 import PlayerFixPage from "@/pages/analysis/PlayerFixPage";
 import FeedbackPage from "@/pages/analysis/FeedbackPage";
 import ReplayPage from "@/pages/analysis/ReplayPage";
-import VideoCommunityPage from "@/pages/community/VideoCommunityPage";
-import TalkCommunityPage from "@/pages/community/TalkCommunityPage";
+import CommunityPage from "@/pages/community/CommunityPage";
 import CommunityRegisterPage from "@/pages/community/CommunityRegisterPage";
 import CommunityEditPage from "@/pages/community/CommunityEditPage";
 import VideoDetailPage from "@/pages/community/VideoDetailPage";
@@ -31,28 +30,30 @@ const AppRoutes = () => (
     <Route path="/" element={<HomePage />} />
     <Route path="*" element={<NotFoundPage />} />
     <Route path="/signup" element={<SignUpPage />} />
-    <Route path="/login" element={<LoginPage/>} />
-    <Route path="/alert" element={<AlertPage/>} />
-    <Route path="/map" element={<MapPage/>} />
-    <Route path="/match" element={<MatchListPage/>} />
-    <Route path="/match/filter" element={<MatchFilterPage/>} />
-    <Route path="/match/:match_id" element={<MatchDetailPage/>} />
-    <Route path="/match/register" element={<MatchRegisterPage/>} />
-    <Route path="/players/:match_id" element={<PlayerFixPage/>} />
-    <Route path="/feedback/:match_id" element={<FeedbackPage/>} />
-    <Route path="/replay/:match_id" element={<ReplayPage/>} />
-    <Route path="/community/video" element={<VideoCommunityPage/>} />
-    <Route path="/community/talk" element={<TalkCommunityPage/>} />
-    <Route path="/community/register" element={<CommunityRegisterPage/>} />
-    <Route path="/community/edit" element={<CommunityEditPage/>} />
-    <Route path="/community/video/:community_id" element={<VideoDetailPage/>} />
-    <Route path="/community/talk/:community_id" element={<TalkDetailPage/>} />
-    <Route path="/club" element={<ClubListPage/>} />
-    <Route path="/club/:club_id" element={<ClubDetailPage/>} />
-    <Route path="/club/register" element={<ClubRegisterPage/>} />
-    <Route path="/club/edit/:club_id" element={<ClubEditPage/>} />
-    <Route path="/profile/:user_id" element={<ProfilePage/>} />
-    <Route path="/profile/edit/:user_id" element={<ProfileEditPage/>} />
+    <Route path="/login" element={<LoginPage />} />
+    <Route path="/alert" element={<AlertPage />} />
+    <Route path="/map" element={<MapPage />} />
+    <Route path="/match" element={<MatchListPage />} />
+    <Route path="/match/filter" element={<MatchFilterPage />} />
+    <Route path="/match/:match_id" element={<MatchDetailPage />} />
+    <Route path="/match/register" element={<MatchRegisterPage />} />
+    <Route path="/players/:match_id" element={<PlayerFixPage />} />
+    <Route path="/feedback/:match_id" element={<FeedbackPage />} />
+    <Route path="/replay/:match_id" element={<ReplayPage />} />
+    <Route path="/community" element={<CommunityPage />} />
+    <Route path="/community/register" element={<CommunityRegisterPage />} />
+    <Route path="/community/edit" element={<CommunityEditPage />} />
+    <Route
+      path="/community/video/:community_id"
+      element={<VideoDetailPage />}
+    />
+    <Route path="/community/talk/:community_id" element={<TalkDetailPage />} />
+    <Route path="/club" element={<ClubListPage />} />
+    <Route path="/club/:club_id" element={<ClubDetailPage />} />
+    <Route path="/club/register" element={<ClubRegisterPage />} />
+    <Route path="/club/edit/:club_id" element={<ClubEditPage />} />
+    <Route path="/profile/:user_id" element={<ProfilePage />} />
+    <Route path="/profile/edit/:user_id" element={<ProfileEditPage />} />
   </Routes>
 );
 
