fc3b41892b8a0cb0873e045e22ed66fc52a4a1b2
예준성 <306yyy@naver.com>
2024-03-18 17:06:23 +0900

    Feat : 개인 기록 스텟 관련 API 작성
    - 기록 누적 API
    - 개인 총합 누적 스텟 조회
    - 개인 경기당 평균 스텟 조회

---
+++ user/src/main/java/com/mancity/user/stat/presentation/StatController.java
@@ -0,0 +1,40 @@
+package com.mancity.user.stat.presentation;
+
+import com.mancity.user.stat.application.StatService;
+import com.mancity.user.stat.application.dto.request.PlusRequestDto;
+import com.mancity.user.stat.application.dto.response.StatAvgResponseDto;
+import com.mancity.user.stat.application.dto.response.StatTotalResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class StatController {
+
+    private final StatService statService;
+
+    // 스텟 누적
+    @PostMapping("/stat/plus")
+    public ResponseEntity<?> plus(@RequestBody PlusRequestDto dto){
+        statService.plus(dto);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    // 경기당 평균 스텟 불러오기, 오각형 그거 아님
+    @GetMapping("/stat/avg/{id}")
+    public ResponseEntity<StatAvgResponseDto> findAvgById(@PathVariable("id") Long id){
+        return new ResponseEntity<>(statService.findAvgById(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/stat/total/{id}")
+    public ResponseEntity<StatTotalResponseDto> findTotalById(@PathVariable("id") Long id){
+        return new ResponseEntity<>(statService.findTotalById(id), HttpStatus.OK);
+    }
+
+    // 스텟 삭제
+    // 어떤 클럽의 종합 스텟
+    // 해당 매치의 종합 스텟
+    // 경기당 스텟
+}
