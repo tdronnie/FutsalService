437de708a6389e7c785b725613417ceb612006a6
Jiyong <aop8021@naver.com>
2024-03-18 22:55:25 +0900

    Style: 클럽/용병 header 생성

---
+++ frontend/mancity-project/src/components/templates/ClubListTemplete.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '@/components/organisms/header/Header'
+
+const ClubListTemplete = () => {
+  return (
+    <>
+        <Header label='용병/클럽' display='hidden' />
+    </>
+  )
+}
+
+export default ClubListTemplete
