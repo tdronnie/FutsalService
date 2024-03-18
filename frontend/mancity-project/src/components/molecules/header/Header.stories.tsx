e28a92478f33ef11b23cf80ea3b95b287e64e43f
choiseongho <chltjdgh3@naver.com>
2024-03-18 14:06:47 +0900

    Fix: storybook  autodocs와 router 전역으로 설정

--- frontend/mancity-project/src/components/molecules/header/Header.stories.tsx
+++ frontend/mancity-project/src/components/molecules/header/Header.stories.tsx
@@ -1,11 +1,9 @@
 import Header from "@/components/molecules/header/Header";
 import type { Meta, StoryObj } from "@storybook/react";
-import { withRouter } from "storybook-addon-remix-react-router";
 
 const meta: Meta<typeof Header> = {
   title: "Components/molecules/Header",
   tags: ["autodocs"],
-  decorators: [withRouter],
   component: Header,
 };
 
