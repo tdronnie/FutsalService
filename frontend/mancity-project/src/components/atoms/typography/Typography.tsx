8142be58ec4d8bf474f9ea919d674a55efcc39e4
choiseongho <chltjdgh3@naver.com>
2024-03-19 01:01:07 +0900

    Feat: Global Card molecule 컴포넌트 및 스토리북 생성

--- frontend/mancity-project/src/components/atoms/typography/Typography.tsx
+++ frontend/mancity-project/src/components/atoms/typography/Typography.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Typography = (props: TailwindPropsType) => {
   const { textSize, fontWeight, textColor, label } = props;
   return <p className={`${textSize} ${fontWeight} ${textColor}`}>{label}</p>;
---
+++ frontend/mancity-project/src/components/molecules/global_card/GlobalCard.tsx
@@ -0,0 +1,27 @@
+import ContentBox from "@/components/atoms/content_box/ContentBox";
+import Typography from "@/components/atoms/typography/Typography";
+
+const GlobalCard = (props: GlobalCardProps) => {
+  const { subTitle, mainTitle, bgimg } = props;
+  return (
+    <div className="border-2 w-44 h-56 rounded-lg shadow-nav">
+      <div className="">
+        <ContentBox width="w-full" height="h-40" bgimg={bgimg} />
+      </div>
+      <div className="mx-2 my-1">
+        <div className="my-1">
+          <Typography label={subTitle} textSize="text-sm" />
+        </div>
+        <div className="-mt-1">
+          <Typography
+            label={mainTitle}
+            textSize="text-lg"
+            fontWeight="font-medium"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GlobalCard;
