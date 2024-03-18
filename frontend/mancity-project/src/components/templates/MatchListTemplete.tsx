768eee813cf70a444a4299c266c1f4b1067c9a93
Jiyong <aop8021@naver.com>
2024-03-18 22:55:51 +0900

    Style: 매치 header 뒤로가기 버튼 숨김

--- frontend/mancity-project/src/components/templates/MatchListTemplete.tsx
+++ frontend/mancity-project/src/components/templates/MatchListTemplete.tsx
@@ -6,7 +6,7 @@ import IconButton from '../atoms/icon_button/IconButton'
 const MatchListTemplete = () => {
   return (
     <>
-        <Header label='매치 목록' />
+        <Header label='매치 목록' display='hidden' />
         <WideCard />
         <IconButton icon="pen" />
     </>
