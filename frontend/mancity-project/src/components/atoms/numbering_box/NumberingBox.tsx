1c25de4eafc6c25cfc862a69f737dcb3c888096b
Jiyong <aop8021@naver.com>
2024-03-18 15:39:13 +0900

    Style: numbering box 배경색 추가

--- frontend/mancity-project/src/components/atoms/numbering_box/NumberingBox.tsx
+++ frontend/mancity-project/src/components/atoms/numbering_box/NumberingBox.tsx
@@ -7,7 +7,7 @@ interface propsType {
 const NumberingBox = (props: propsType) => {
   const { number } = props;
   return (
-    <div className="w-10 h-5 border-2 border-mancity font-medium text-xs text-center">
+    <div className="w-10 h-5 text-xs font-medium text-center bg-white border-2 border-mancity">
       # {number}
     </div>
   );
