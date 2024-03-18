a985b50b096219c996a33e2518899c666af7a51c
choiseongho <chltjdgh3@naver.com>
2024-03-19 01:02:15 +0900

    Feat: Highlight Card List organism 컴포넌트 및 스토리북 생성

---
+++ frontend/mancity-project/src/components/molecules/highlight_card/HighlightCard.tsx
@@ -0,0 +1,28 @@
+import ContentBox from "@/components/atoms/content_box/ContentBox";
+import FontawsomeIcon from "@/components/atoms/fontawsome_icon/FontawsomeIcon";
+import Typography from "@/components/atoms/typography/Typography";
+
+const HighlightCard = (props: HighlightCardProps) => {
+  const { mainTitle, bgimg } = props;
+  return (
+    <div className="border-2 w-44 h-54 rounded-lg shadow-nav">
+      <div className="">
+        <ContentBox width="w-full" height="h-40" bgimg={bgimg} />
+      </div>
+      <div className="flex justify-around m-2 ">
+        <div className="">
+          <Typography
+            label={mainTitle}
+            textSize="text-lg"
+            fontWeight="font-medium"
+          />
+        </div>
+        <div className="mt-[2px]">
+          <FontawsomeIcon icon="bookmark" size="1x" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HighlightCard;
---
+++ frontend/mancity-project/src/components/organisms/group_highlight_card/GroupHighlightCard.tsx
@@ -0,0 +1,51 @@
+import Typography from "@/components/atoms/typography/Typography";
+import HighlightCard from "@/components/molecules/highlight_card/HighlightCard";
+
+const GroupHighlightCard = () => {
+  return (
+    <div className="justify-center">
+      <div className="m-4">
+        <Typography
+          label="MY 하이라이트"
+          textColor="text-sofcity"
+          textSize="text-2xl"
+          fontWeight="font-semibold"
+        />
+      </div>
+      <div className="flex overflow-x-scroll scrollbar-hide mx-2 ">
+        <div className="m-2">
+          <HighlightCard
+            mainTitle="광주 신화 풋살장"
+            bgimg="bg-[url('/favicon.ico')]"
+          />
+        </div>
+        <div className="m-2">
+          <HighlightCard
+            mainTitle="광주 신화 풋살장"
+            bgimg="bg-[url('/favicon.ico')]"
+          />
+        </div>
+        <div className="m-2">
+          <HighlightCard
+            mainTitle="광주 신화 풋살장"
+            bgimg="bg-[url('/favicon.ico')]"
+          />
+        </div>
+        <div className="m-2">
+          <HighlightCard
+            mainTitle="광주 신화 풋살장"
+            bgimg="bg-[url('/favicon.ico')]"
+          />
+        </div>
+        <div className="m-2">
+          <HighlightCard
+            mainTitle="광주 신화 풋살장"
+            bgimg="bg-[url('/favicon.ico')]"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GroupHighlightCard;
