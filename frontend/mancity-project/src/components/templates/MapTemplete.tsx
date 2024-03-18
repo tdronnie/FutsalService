17d79344874dc341300db9014303849cde253b6b
Jiyong <aop8021@naver.com>
2024-03-18 22:55:32 +0900

    Style: 지도 header 생성

---
+++ frontend/mancity-project/src/components/templates/MapTemplete.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '@/components/organisms/header/Header'
+
+const MapTemplete = () => {
+  return (
+    <>
+        <Header label='지도 보기' display='hidden' />
+    </>
+  )
+}
+
+export default MapTemplete
