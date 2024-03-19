75f8fbbf8bb316577da1ab25da1798a6d719c41a
예준성 <306yyy@naver.com>
2024-03-19 20:49:46 +0900

    Feat : player 기록 저장 위한 클래스 작성

---
+++ social/src/main/java/com/mancity/social/match/domain/Player.java
@@ -0,0 +1,36 @@
+package com.mancity.social.match.domain;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Embeddable
+public class Player {
+
+    private String nickname;
+
+    private int speed; // 최고속도
+
+    private int distanceCovered; // 총 이동 거리
+
+    private int pass; // 총 패스 횟수
+
+    private int shotsOnTarget; // 총 유효 슈팅 수
+
+    private int shot; // 총 슈팅 횟수
+
+    private int goal; // 총 득점 수
+
+    private int assist; // 총 어시스트 수
+
+    private int turnOverInOffense; // 공격 시 턴오버 당한 총 횟수
+
+    private int turnOverInDefense; // 수비 시 턴오버 성공한 총 횟수
+
+}
