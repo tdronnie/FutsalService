c41c033a13c3f5a9645133ba3e33250284e40274
choiseongho <chltjdgh3@naver.com>
2024-03-19 16:24:32 +0900

    Feat: header style 수정

--- frontend/mancity-project/src/components/organisms/header/Header.tsx
+++ frontend/mancity-project/src/components/organisms/header/Header.tsx
@@ -1,25 +1,35 @@
 import FontawsomeIcon from "@/components/atoms/fontawsome_icon/FontawsomeIcon";
+import SubmitButton from "@/components/atoms/submit_button/SubmitButton";
 import Typography from "@/components/atoms/typography/Typography";
 import { useNavigate } from "react-router-dom";
 
 const Header = (props: HeaderPropsType) => {
-  const { label, display } = props;
+  const { label, backArrow, headerButton } = props;
   const navigate = useNavigate();
   const onClickBackArrow = () => {
     navigate(-1);
   };
   return (
-    <div className="flex p-1 ">
-      <div className={`p-1 ${display}`} onClick={onClickBackArrow}>
-        <FontawsomeIcon icon="arrow-left" size="1x" color="#5D7A93" />
+    <div className="flex p-2 mb-2 mx-1 sticky top-0 bg-white min-w-full ">
+      <div
+        className={`p-1 ${backArrow ? "visible" : "invisible"} min-w-16 text-[1.4rem]`}
+        onClick={onClickBackArrow}
+      >
+        <FontawsomeIcon icon="arrow-left" color="#5D7A93" />
       </div>
-      <div className="flex-grow mr-8 text-center">
+      <div className="flex-grow text-center">
         <Typography
-          textSize="text-xl"
+          textSize="text-3xl"
           fontWeight="font-medium"
           textColor="text-sofcity"
           label={label}
         />
+      </div>
+      <div
+        className={`mt-1
+      ${headerButton ? "visible" : "invisible"} `}
+      >
+        <SubmitButton label="작성하기" />
       </div>
     </div>
   );
