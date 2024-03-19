be9e5ad32314657562a86001893404c4d3922936
예준성 <306yyy@naver.com>
2024-03-19 20:44:34 +0900

    Feat : match id를 통해 비디오 업로드 요청 dto 작성

---
+++ social/src/main/java/com/mancity/social/match/application/dto/request/MatchVideoUploadDto.java
@@ -0,0 +1,13 @@
+package com.mancity.social.match.application.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MatchVideoUploadDto {
+
+    private long id;
+}
