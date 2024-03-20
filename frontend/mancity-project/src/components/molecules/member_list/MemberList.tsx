1137b3ac633e7e7759a3710d6e092b6d8d7c6d82
choiseongho <chltjdgh3@naver.com>
2024-03-20 17:03:29 +0900

    Feat: member 리스트 molecule 생성

---
+++ frontend/mancity-project/src/components/molecules/member_list/MemberList.tsx
@@ -0,0 +1,46 @@
+import ContentBox from "@/components/atoms/content_box/ContentBox";
+import IconButton from "@/components/atoms/icon_button/IconButton";
+import TypographyLine from "@/components/atoms/typography_line/TypographyLine";
+
+const MemberList = (props: MemberListPropsType) => {
+  const file = "/src/assets/imgs/go_to_analysis.jpg";
+  const { label } = props;
+  return (
+    <div className="">
+      <div>
+        <TypographyLine label={label} />
+      </div>
+      <div className="flex  m-2">
+        <div className="mr-2">
+          <ContentBox
+            width="w-14"
+            height="h-14"
+            rounded="rounded-full"
+            file={file}
+          />
+        </div>
+        <div className="mr-2">
+          <ContentBox
+            width="w-14"
+            height="h-14"
+            rounded="rounded-full"
+            file={file}
+          />
+        </div>
+        <div className="mr-2">
+          <ContentBox
+            width="w-14"
+            height="h-14"
+            rounded="rounded-full"
+            file={file}
+          />
+        </div>
+        <div className="mr-2">
+          <IconButton icon="plus" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MemberList;
