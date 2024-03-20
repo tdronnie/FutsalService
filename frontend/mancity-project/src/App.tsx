b2c98eb2210fb2eeacd6348a40f8aeac3b36a062
choiseongho <chltjdgh3@naver.com>
2024-03-20 17:53:53 +0900

    Refactor: navbar 선언식으로 수정

--- frontend/mancity-project/src/App.tsx
+++ frontend/mancity-project/src/App.tsx
@@ -8,6 +8,34 @@ import tooSmallImage from "./assets/imgs/toosmall.png";
 import Navbar from "./components/molecules/navbar/Navbar";
 
 const App = () => {
+  const ConditionalBottomNav = () => {
+    const location = useLocation();
+    const currentPathname = location.pathname;
+
+    // Navbar 사용하지 않는 페이지 예외 처리 코드
+    if (
+      location.pathname === "/login" ||
+      location.pathname === "/signup" ||
+      location.pathname === "/alert" ||
+      location.pathname === "/map" ||
+      location.pathname === "/match/register" ||
+      location.pathname === "/community/register" ||
+      location.pathname === "/community/edit" ||
+      location.pathname === "/club/register" ||
+      matchPath("/match/:match_id", currentPathname) ||
+      matchPath("/replay/:match_id", currentPathname) ||
+      matchPath("/community/video/:community_id", currentPathname) ||
+      matchPath("/community/talk/:community_id", currentPathname) ||
+      matchPath("/club/:club_id", currentPathname) ||
+      matchPath("/club/edit/:club_id", currentPathname) ||
+      matchPath("/players/:match_id", currentPathname)
+    ) {
+      return null;
+    }
+
+    return <Navbar />;
+  };
+
   return (
     <>
       {/* 화면 크기 너무 작으면 서비스 이용 제한 */}
@@ -28,31 +56,3 @@ const App = () => {
 };
 
 export default App;
-
-function ConditionalBottomNav() {
-  const location = useLocation();
-  const currentPathname = location.pathname;
-
-  // Navbar 사용하지 않는 페이지 예외 처리 코드
-  if (
-    location.pathname === "/login" ||
-    location.pathname === "/signup" ||
-    location.pathname === "/alert" ||
-    location.pathname === "/map" ||
-    location.pathname === "/match/register" ||
-    location.pathname === "/community/register" ||
-    location.pathname === "/community/edit" ||
-    location.pathname === "/club/register" ||
-    matchPath("/match/:match_id", currentPathname) ||
-    matchPath("/replay/:match_id", currentPathname) ||
-    matchPath("/community/video/:community_id", currentPathname) ||
-    matchPath("/community/talk/:community_id", currentPathname) ||
-    matchPath("/club/:club_id", currentPathname) ||
-    matchPath("/club/edit/:club_id", currentPathname) ||
-    matchPath("/players/:match_id", currentPathname)
-  ) {
-    return null;
-  }
-
-  return <Navbar />;
-}
