1b321b4c276b5cc7bfe172bbfd48954d1883f12a
Jiyong <aop8021@naver.com>
2024-03-21 02:01:16 +0900

    Feat: 클럽상세 헤더 설정 완료

---
+++ frontend/mancity-project/src/components/templates/ClubDetailTemplete.tsx
@@ -0,0 +1,11 @@
+import Header from "../organisms/header/Header";
+
+const ClubDetailTemplete = () => {
+  return (
+    <>
+      <Header label="클럽 상세" backArrow={true} headerButton={false} />
+    </>
+  );
+};
+
+export default ClubDetailTemplete;
