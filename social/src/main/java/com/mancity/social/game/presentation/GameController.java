6284ccc7c9506dbfe34eae46b789d68fb100daf0
예준성 <306yyy@naver.com>
2024-03-20 14:46:48 +0900

    Feat : 개인 경기 데이터 User Service의 Stat에 할당하는 메소드 구현

--- social/src/main/java/com/mancity/social/game/presentation/GameController.java
+++ social/src/main/java/com/mancity/social/game/presentation/GameController.java
@@ -1,12 +1,10 @@
 package com.mancity.social.game.presentation;
 
 import com.mancity.social.game.application.GameService;
-import com.mancity.social.game.application.dto.request.CheckManagerDto;
-import com.mancity.social.game.application.dto.request.GameCreateRequestDto;
-import com.mancity.social.game.application.dto.request.GameDataInputDto;
-import com.mancity.social.game.application.dto.request.GameVideoUploadDto;
+import com.mancity.social.game.application.dto.request.*;
 import com.mancity.social.game.application.dto.response.GameResponseDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,20 +13,19 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/match")
+@RequestMapping("/api/game")
 @RequiredArgsConstructor
+@Slf4j
 public class GameController {
 
     private final GameService gameService;
 
-    // 매치 생성
     @PostMapping("/create")
     public ResponseEntity<?> create(@RequestBody GameCreateRequestDto dto) {
         gameService.create(dto);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
-    // 동영상 업로드
     @PutMapping("/upload")
     public ResponseEntity<GameResponseDto> upload(@RequestPart(value = "files", required = false) List<MultipartFile> files,
                                                   @RequestPart(value = "dto") GameVideoUploadDto dto) {
@@ -46,32 +43,24 @@ public class GameController {
     ResponseEntity<List<GameResponseDto>> findAllByUserId(@PathVariable long id){
         return new ResponseEntity<>(gameService.findAllByUserId(id), HttpStatus.OK);
     }
-    // 매치 분석 완료 후 데이터 할당 -> 팀데이터, 개인데이터에 각각 저장
 
-
-    // calc 에서 완료 후 보내줄 api
     @PostMapping("/input")
-    public ResponseEntity<?> inputData(@RequestBody GameDataInputDto dto){
+    public ResponseEntity<?> inputDataFromCalc(@RequestBody GameDataInputDto dto){
+        log.info("CALC SERVER : INPUT DATA {}", dto);
         gameService.inputData(dto);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    // 개인데이터 guest 인것 할당
-    /** input : post, 개인 닉네임, 매치 아이디, 팀 구분, 플레이어 번호
-     * logic : match 조회 후 닉네임을 해당 match 의 팀 - 플레이어의 string name 에 할당
-     * output : 200 OK
-     * */
+    @PostMapping("/allocate")
+    public ResponseEntity<?> allocateData(@RequestBody GameDataAllocateDto dto){
+        gameService.allocateData(dto);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
-    // 팀 피드백
-
-    // 매치장 유저가 맞는지 확인
     @GetMapping("/match/check/{match}/{user}")
     public ResponseEntity<Boolean> checkManager(@PathVariable(name = "match") long matchId,
                                                 @PathVariable(name = "user") long userId) {
         return new ResponseEntity<>(gameService.checkManager(CheckManagerDto.of(matchId, userId)), HttpStatus.OK);
     }
 
-    // 회원이 참여한 매치가 맞는지
-
-    //
 }
