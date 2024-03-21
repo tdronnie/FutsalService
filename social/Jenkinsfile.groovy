fa1b35b7bd1fbeb3938d7344fc6c3e4d1da5244a
이세은 <senyvee9@gmail.com>
2024-03-21 15:55:26 +0900

    Chore: returnStatus에서 returnStdout으로 변경

--- social/Jenkinsfile.groovy
+++ social/Jenkinsfile.groovy
@@ -58,8 +58,8 @@ pipeline {
             steps {
                 script {
 //                    def containerInfo = sh(script: "docker inspect ${CONTAINER_NAME}", returnStatus: true)
-                    def  socialContainerExists = sh(script: "docker ps --filter=name=${CONTAINER_NAME}", returnStatus: true) == 0
-                    if (socialContainerExists) {
+                    def  socialContainerExists = sh(script: "docker ps --filter=name=${CONTAINER_NAME}", returnStdout: true).trim()
+                    if (socialContainerExists == 0) {
                         sh "docker stop ${CONTAINER_NAME}"
                         sh "docker rm ${CONTAINER_NAME}"
                     } else {
