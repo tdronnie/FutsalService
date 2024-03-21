86c0c865303b4e4f3d741cd4978cbeaa5a7c74f1
Jiyong <aop8021@naver.com>
2024-03-21 15:54:01 +0900

    Feat: tab 기본값 설정 옵션 추가

--- frontend/mancity-project/src/components/atoms/shadcn_tabs/ShadcnTabs.tsx
+++ frontend/mancity-project/src/components/atoms/shadcn_tabs/ShadcnTabs.tsx
@@ -3,13 +3,14 @@ import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 interface PropsType {
   Tab1: string;
   Tab2: string;
+  defaultTab: string;
   onChange: () => void;
 }
 
 const ShadcnTabs = (props: PropsType) => {
-  const { Tab1, Tab2, onChange } = props;
+  const { Tab1, Tab2, defaultTab, onChange } = props;
   return (
-    <Tabs defaultValue="tab1" onValueChange={onChange} className="w-80">
+    <Tabs defaultValue={defaultTab} onValueChange={onChange} className="flex-grow">
       <TabsList>
         <TabsTrigger value="tab1">{Tab1}</TabsTrigger>
         <TabsTrigger value="tab2">{Tab2}</TabsTrigger>
