1cff70548e6d6111517264e2fd02a23ad613ef35
Jiyong <aop8021@naver.com>
2024-03-21 11:18:37 +0900

    Style: 알림페이지 디자인 완료

---
+++ frontend/mancity-project/src/components/molecules/alert_card/AlertCard.tsx
@@ -0,0 +1,46 @@
+import ContentBox from "@/components/atoms/content_box/ContentBox";
+import SubButton from "@/components/atoms/sub_button/SubButton";
+import Typography from "@/components/atoms/typography/Typography";
+
+const AlertCard = (props: WideCardPropsType) => {
+  const { maintext, subtext, minitext, buttonlabel } = props;
+  return (
+    <>
+      <div className="p-2 m-3 rounded-md shadow-nav">
+        <div className="flex flex-col justify-around">
+          <div className="p-1">
+            <Typography
+              fontWeight="font-medium"
+              label={maintext}
+              textColor="text-black"
+              textSize="text-xl"
+            />
+            <div className="mt-1">
+              <Typography
+                fontWeight="font-medium"
+                label={subtext}
+                textColor="text-sofcity"
+                textSize="text-md"
+              />
+            </div>
+          </div>
+          <div className="flex">
+          <div className="mt-3">
+            <Typography
+              fontWeight="font-medium"
+              label={minitext}
+              textSize="text-sm"
+              textColor="text-sofcity"
+            />
+          </div>
+        <div className="mt-auto ml-auto">
+          <SubButton label={buttonlabel} hover={true} />
+        </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AlertCard;
--- frontend/mancity-project/src/components/templates/AlertTemplate.tsx
+++ frontend/mancity-project/src/components/templates/AlertTemplate.tsx
@@ -1,9 +1,16 @@
 import Header from "@/components/organisms/header/Header";
+import AlertCard from "../molecules/alert_card/AlertCard";
 
 const AlertTemplate = () => {
   return (
     <>
       <Header label="주요 알림" backArrow={true} headerButton={false} />
+      <AlertCard
+        maintext="📫 용병 요청이 도착했어요"
+        subtext="요청을 수락하고 경기를 뛰어보세요!"
+        minitext="알림 날짜: 2024년 03월 11일"
+        buttonlabel="바로가기"
+      />
     </>
   );
 };
