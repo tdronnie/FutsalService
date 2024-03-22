33128e2de72e0795d6af97cb939e9b2d96abdb2d
예준성 <306yyy@naver.com>
2024-03-22 14:13:59 +0900

    refactor : swagger library 임포트 추가

--- user/src/main/java/com/mancity/user/common/swagger/config/SwaggerConfig.java
+++ user/src/main/java/com/mancity/user/common/swagger/config/SwaggerConfig.java
@@ -4,6 +4,8 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.info.Info;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
