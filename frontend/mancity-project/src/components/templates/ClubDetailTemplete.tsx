8e9d49a05310ec44cf3196067b4ba99ec51e17fd
Jiyong <aop8021@naver.com>
2024-03-22 11:46:17 +0900

    Style: 클럽 상세 페이지 디자인 완성

--- frontend/mancity-project/src/components/templates/ClubDetailTemplete.tsx
+++ frontend/mancity-project/src/components/templates/ClubDetailTemplete.tsx
@@ -1,9 +1,22 @@
+import ClubDetailBody from "../organisms/club_detail_body/ClubDetailBody";
+import Footer from "../organisms/footer/Footer";
 import Header from "../organisms/header/Header";
+import { useNavigate } from "react-router-dom";
 
 const ClubDetailTemplete = () => {
+  const navigate = useNavigate();
+  const onButtonClick = () => {
+    navigate("/");
+  };
   return (
     <>
       <Header label="클럽 상세" backArrow={true} headerButton={false} />
+      <ClubDetailBody />
+      <Footer
+        label="1800점 ∙ 36명/50명"
+        buttonLabel="클럽 참여 신청"
+        onButtonClick={onButtonClick}
+      />
     </>
   );
 };
