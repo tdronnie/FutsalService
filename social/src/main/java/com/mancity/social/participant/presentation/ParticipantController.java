66b4565454d5c441561479866ab4805fbfbd448c
예준성 <306yyy@naver.com>
2024-03-24 16:12:20 +0900

    Refactor : 조회 요청시 parameter 값 변경

--- social/src/main/java/com/mancity/social/participant/presentation/ParticipantController.java
+++ social/src/main/java/com/mancity/social/participant/presentation/ParticipantController.java
@@ -33,14 +33,14 @@ public class ParticipantController {
     }
 
     // 현재 참여자 조회
-    @GetMapping("/{id}")
-    public ResponseEntity<List<ParticipantResponseDto>> findParticipantsByGameId(@PathVariable("id") long id){
+    @GetMapping("/{gameId}")
+    public ResponseEntity<List<ParticipantResponseDto>> findParticipantsByGameId(@PathVariable("gameId") long id){
         return new ResponseEntity<>(participantService.findParticipantsByGameId(id), HttpStatus.OK);
     }
 
     // 해당매치의 참여요청조회
-    @GetMapping("/request/{id}")
-    public ResponseEntity<List<ParticipateRequestResponseDto>> findRequestsByGameId(@PathVariable("id") long id){
+    @GetMapping("/request/{gameId}")
+    public ResponseEntity<List<ParticipateRequestResponseDto>> findRequestsByGameId(@PathVariable("gameId") long id){
         return new ResponseEntity<>(participantService.findRequestsByGameId(id), HttpStatus.OK);
     }
 }
