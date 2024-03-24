2010db9dddc6785ea5421c5c4609bb795aabab57
예준성 <306yyy@naver.com>
2024-03-25 02:45:24 +0900

    Feat : Social 서버 swagger 적용

---
+++ social/src/main/java/com/mancity/social/common/swagger/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.mancity.social.common.swagger;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(title = "Social API Document", description = "SOCIAL SERVICE", version = "v3")
+)
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+
+        return new OpenAPI()
+                .addServersItem(new Server().url("/"));
+    }
+
+}
