5ffd99f73675b416ecfa62e2b7cf7ab69ae1ea7c
예준성 <306yyy@naver.com>
2024-03-24 11:36:31 +0900

    Feat : 참여자에 대한 entity 작성

---
+++ social/src/main/java/com/mancity/social/participant/domain/Participant.java
@@ -0,0 +1,27 @@
+package com.mancity.social.participant.domain;
+
+import com.mancity.social.game.domain.Game;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+public class Participant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn
+    private Game game;
+
+    private Long userId;
+
+}
