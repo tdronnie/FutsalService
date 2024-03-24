531c0ff71ab859969c686299ebc84a38234ca396
choiseongho <chltjdgh3@naver.com>
2024-03-25 00:47:47 +0900

    Feat: 이메일, 닉네임 유효성 로직 inputGroup에 추가

--- frontend/mancity-project/src/components/molecules/input_group/InputGroup.tsx
+++ frontend/mancity-project/src/components/molecules/input_group/InputGroup.tsx
@@ -1,6 +1,8 @@
+import { fetchCheckEmail, fetchCheckNickname } from "@/apis/userApis";
 import GlobalInput from "@/components/atoms/global_input/GlobalInput";
 import SubButton from "@/components/atoms/sub_button/SubButton";
 import Typography from "@/components/atoms/typography/Typography";
+import { useMutation } from "@tanstack/react-query";
 
 const InputGroup = (props: InputGroupPropsType) => {
   const {
@@ -11,6 +13,47 @@ const InputGroup = (props: InputGroupPropsType) => {
     textValue,
     setTextValue,
   } = props;
+
+  // 이메일 유효성 로직
+  const { mutate: emailMutate } = useMutation({
+    mutationFn: fetchCheckEmail,
+    onSuccess(result: boolean) {
+      if (result) {
+        console.log("사용 가능한 이메일입니다.");
+      } else {
+        console.log("사용할 수 없는 이메일입니다.");
+      }
+    },
+    onError(error) {
+      console.log(error);
+    },
+  });
+
+  // 닉네임 유효성 로직
+  const { mutate: nicknameMutate } = useMutation({
+    mutationFn: fetchCheckNickname,
+    onSuccess(result: boolean) {
+      if (result) {
+        console.log("사용 가능한 닉네임입니다.");
+      } else {
+        console.log("사용할 수 없는 닉네임입니다.");
+      }
+    },
+    onError(error) {
+      console.log(error);
+    },
+  });
+
+  const handleCheckingClick = () => {
+    if (textValue) {
+      if (typographyLabel === "이메일") {
+        emailMutate(textValue);
+      } else if (typographyLabel === "닉네임") {
+        nicknameMutate(textValue);
+      }
+    }
+  };
+
   return (
     <div className="flex m-4">
       <div className="w-full">
@@ -27,8 +70,14 @@ const InputGroup = (props: InputGroupPropsType) => {
           setTextValue={setTextValue}
         />
       </div>
+      {/* checkingLabel 존재하면 단순 입력, 존재하지 않으면 중복 확인 */}
       {checking && (
-        <div className="mt-5">
+        <div
+          className="mt-5"
+          onClick={
+            checkingLabel === "중복 확인" ? handleCheckingClick : undefined
+          }
+        >
           <SubButton label={checkingLabel} />
         </div>
       )}
