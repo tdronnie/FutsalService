607c3f6390f99f9f8f6a624c9146a69033f4ff0d
예준성 <306yyy@naver.com>
2024-03-25 09:30:27 +0900

    Feat : CORS 문제 해결 시도

--- user/src/main/java/com/mancity/user/user/presentation/UserController.java
+++ user/src/main/java/com/mancity/user/user/presentation/UserController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/api/user")
 @Slf4j
+@CrossOrigin(origins = {"http://localhost:3000"})
 public class UserController {
 
     private final UserService userService;
