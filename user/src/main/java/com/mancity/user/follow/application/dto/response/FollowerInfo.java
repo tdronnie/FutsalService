b910635ad3fbeec9a17c626f9b958fdeed60fdf5
예준성 <306yyy@naver.com>
2024-03-26 11:05:53 +0900

    Feat : 각각 팔로잉/팔로워 유저 정보 담아줄 DTO 작성

---
+++ user/src/main/java/com/mancity/user/follow/application/dto/response/FollowerInfo.java
@@ -0,0 +1,33 @@
+package com.mancity.user.follow.application.dto.response;
+
+import com.mancity.user.stat.domain.MainStat;
+import com.mancity.user.user.domain.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class FollowerInfo {
+
+    private long id;
+
+    private String nickname;
+
+    private String profileImage;
+
+    private double overall;
+
+    public static FollowerInfo of(User user, MainStat mainStat){
+        return FollowerInfo.builder()
+                .id(user.getId())
+                .nickname(user.getNickName())
+//                .profileImage(user.profileImage())
+                .profileImage("이세은님이 추가하실겁니다.")
+                .overall(mainStat.getOverall())
+                .build();
+    }
+}
