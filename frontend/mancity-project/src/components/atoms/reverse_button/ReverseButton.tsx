22fe673360a326377b0657646e9e53c7df1a0f62
Jiyong <aop8021@naver.com>
2024-03-26 12:09:04 +0900

    Style: 입장 페이지 버튼 호버 제거 및 스타일 조정

--- frontend/mancity-project/src/components/atoms/reverse_button/ReverseButton.tsx
+++ frontend/mancity-project/src/components/atoms/reverse_button/ReverseButton.tsx
@@ -1,12 +1,12 @@
 const ReverseButton = (props: TailwindPropsType) => {
-  const { width, label, isdisabled } = props;
+  const { width, label, isdisabled, hover } = props;
   return (
     <button
       type="button"
       disabled={!isdisabled}
       className={`${width}
       font-medium text-sm h-10 rounded-lg bg-white text-mancity border-mancity border-2
-      ${!isdisabled ? "opacity-60" : " hover:bg-mancity hover:text-white"}`}
+      ${hover ? (!isdisabled ? "opacity-60" : " hover:bg-mancity hover:text-white"):"block"}`}
     >
       {label}
     </button>
--- frontend/mancity-project/src/pages/EntryPage.tsx
+++ frontend/mancity-project/src/pages/EntryPage.tsx
@@ -3,10 +3,10 @@ import ReverseButton from "@/components/atoms/reverse_button/ReverseButton";
 import { useNavigate } from "react-router-dom";
 
 const EntryPage = () => {
-    const navigate = useNavigate();
-    const handleNavigate = ({ path }: NavigateType) => {
-      navigate(path);
-    };
+  const navigate = useNavigate();
+  const handleNavigate = ({ path }: NavigateType) => {
+    navigate(path);
+  };
   return (
     <div className="flex items-center justify-center min-h-[100vh]">
       <div className="flex flex-col items-center">
@@ -15,15 +15,14 @@ const EntryPage = () => {
           alt="logo"
           className="w-[12.5rem] h-[20rem] mb-5"
         />
-        <div className="m-1"
-                onClick={() => handleNavigate({ path: "/login" })}
-                >
-        <GlobalButton isdisabled label="로그인" width="w-80" />
+        <div className="m-1" onClick={() => handleNavigate({ path: "/login" })}>
+          <GlobalButton isdisabled label="로그인" width="w-80" hover={false} />
         </div>
-        <div className="m-1"
-                onClick={() => handleNavigate({ path: "/signup" })}
-                >
-        <ReverseButton isdisabled label="회원가입" width="w-80" />
+        <div
+          className="m-1"
+          onClick={() => handleNavigate({ path: "/signup" })}
+        >
+          <ReverseButton isdisabled label="회원가입" width="w-80" hover={false} />
         </div>
       </div>
     </div>
