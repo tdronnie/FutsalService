55aa6a0e014e4852511fb9eaff138814ff8a90d4
예준성 <306yyy@naver.com>
2024-03-26 22:26:46 +0900

    Refactor: 언팔로우 쿼리 수정

--- user/src/main/java/com/mancity/user/follow/domain/repository/FollowRepository.java
+++ user/src/main/java/com/mancity/user/follow/domain/repository/FollowRepository.java
@@ -2,6 +2,7 @@ package com.mancity.user.follow.domain.repository;
 
 import com.mancity.user.follow.domain.Follow;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
@@ -14,7 +15,8 @@ public interface FollowRepository extends JpaRepository<Follow, Long> {
     @Query("select f.receiverId from Follow f where senderId=:senderId")
     List<Long> findReceiverIdBySenderId(long senderId);
 
+    @Modifying
     @Query("delete from Follow f where senderId=:senderId and receiverId=:receiverId")
-    void deleteBySenderIdAndReceiverId(long senderId, long receiverId);
+    int deleteBySenderIdAndReceiverId(long senderId, long receiverId);
 
 }
