0b2cd2b00c0108bba94803d71dd1e65908cc2c8f
윤지현 <goon03077@gmail.com>
2024-03-27 15:43:55 +0900

    Feat: flask 서버 통신 구현

--- flask/app.py
+++ flask/app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify, request, make_response
+import time
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Hello, Flask!'
+@app.route("/track", methods=['POST'])
+def track_request():
+    args_dict = request.form.to_dict()
+    # time.sleep(100)
+    source = args_dict.get('url')
+    source = args_dict.get('game_id')
+    return make_response(jsonify(args_dict),200)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
