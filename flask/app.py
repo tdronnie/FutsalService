23c5198f1423f34d531b1b5fc37018f0011c76bc
윤지현 <goon03077@gmail.com>
2024-03-27 15:57:35 +0900

    Test: await 테스트를 위한 대기  코드 추가

--- flask/app.py
+++ flask/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/track", methods=['POST'])
 def track_request():
     args_dict = request.form.to_dict()
-    # time.sleep(100)
+    time.sleep(100)
     source = args_dict.get('url')
     source = args_dict.get('game_id')
     return make_response(jsonify(args_dict),200)
