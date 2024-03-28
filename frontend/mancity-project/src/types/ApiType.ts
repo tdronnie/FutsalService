17f803af012ee6d8469bb2de0ac553c7d8bdaccb
choiseongho <chltjdgh3@naver.com>
2024-03-28 22:35:14 +0900

    Feat: 매치 상세, 생성 type 생성

--- frontend/mancity-project/src/types/ApiType.ts
+++ frontend/mancity-project/src/types/ApiType.ts
@@ -47,3 +47,64 @@ interface followListDataType {
     }[];
   };
 }
+
+// 매치 생성 타입
+interface matchCreateType {
+  replayUrl: string;
+  gender: number;
+  manager: number;
+  startData: Date;
+  time: number;
+  playerNumber: number;
+  level: string;
+  courtId: string;
+  over: boolean;
+}
+
+interface matchDetailPropsDataType extends matchCreateType {
+  gameId: number;
+
+  highlights: string[];
+  participants: {
+    id: number;
+    userId: number;
+  }[];
+  teamA: {
+    goal: number;
+    pass: number;
+    shot: number;
+    shotOnTarget: number;
+  };
+  teamB: {
+    goal: number;
+    pass: number;
+    shot: number;
+    shotOnTarget: number;
+  };
+  playersA: {
+    id: number;
+    nickname: string;
+    speed: number;
+    distanceCovered: number;
+    pass: number;
+    shotsOnTarget: number;
+    shot: number;
+    goal: number;
+    assist: number;
+    turnOverInOffense: number;
+    turnOverInDefense: number;
+  }[];
+  playersB: {
+    id: number;
+    nickname: string;
+    speed: number;
+    distanceCovered: number;
+    pass: number;
+    shotsOnTarget: number;
+    shot: number;
+    goal: number;
+    assist: number;
+    turnOverInOffense: number;
+    turnOverInDefense: number;
+  }[];
+}
