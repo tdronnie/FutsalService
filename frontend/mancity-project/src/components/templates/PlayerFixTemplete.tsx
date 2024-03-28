f2510fdaad7e822e7634b22464280799966eb231
Jiyong <aop8021@naver.com>
2024-03-29 03:02:51 +0900

    Feat: 선수 확정 페이지 디자인 완성

---
+++ frontend/mancity-project/src/components/templates/PlayerFixTemplete.tsx
@@ -0,0 +1,53 @@
+import Header from "@/components/organisms/header/Header";
+import Typography from "@/components/atoms/typography/Typography";
+import PlayerInfo from "../molecules/player_info/PlayerInfo";
+
+const PlayerFixTemplete = () => {
+  return (
+    <div className="pb-4">
+      <Header label="선수 지정" backArrow={true} headerButton={false} />
+        
+      <div className="m-4">
+        <div id="glassui" className="p-1">
+          {/* 선수 지정을 위한 사진 나오는 자리 */}
+          {/* AI단에서 받아오는 사진으로 대체할 예정 */}
+          <div className="flex flex-col justify-center m-3">
+            <video id="myVideo">
+              <source
+                src="https://iandwe.s3.ap-northeast-2.amazonaws.com/match/yKa0ly3L"
+                type="video/mp4"
+              />
+            </video>
+          </div>
+        </div>
+      </div>
+
+      <div id="glassui" className="py-1 mx-4 mt-4">
+      <div className="mx-4 my-2">
+          <Typography
+            label="선수 정보"
+            textColor="black"
+            textSize="text-2xl"
+            fontWeight="font-medium"
+          />
+        </div>
+        <hr className="border border-sofcity" />
+        <PlayerInfo player="선수#1(HOME)" color="text-red-500"/>
+        <PlayerInfo player="선수#2(HOME)" color="text-red-500"/>
+        <PlayerInfo player="선수#3(HOME)" color="text-red-500"/>
+        <PlayerInfo player="선수#4(HOME)" color="text-red-500"/>
+        <PlayerInfo player="선수#5(HOME)" color="text-red-500"/>
+        <PlayerInfo player="선수#6(HOME)" color="text-red-500"/>
+        <PlayerInfo player="선수#1(AWAY)" color="text-blue-600"/>
+        <PlayerInfo player="선수#2(AWAY)" color="text-blue-600"/>
+        <PlayerInfo player="선수#3(AWAY)" color="text-blue-600"/>
+        <PlayerInfo player="선수#4(AWAY)" color="text-blue-600"/>
+        <PlayerInfo player="선수#5(AWAY)" color="text-blue-600"/>
+        <PlayerInfo player="선수#6(AWAY)" color="text-blue-600"/>
+      </div>
+
+    </div>
+  )
+}
+
+export default PlayerFixTemplete
