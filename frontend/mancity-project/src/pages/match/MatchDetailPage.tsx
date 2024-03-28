573a60cf8e29f94a6fd1f7c4d4b44c49cc5d01c9
choiseongho <chltjdgh3@naver.com>
2024-03-28 22:37:50 +0900

    Feat: 매치 상세 페이지 get api 로직 생성

--- frontend/mancity-project/src/pages/match/MatchDetailPage.tsx
+++ frontend/mancity-project/src/pages/match/MatchDetailPage.tsx
@@ -1,9 +1,23 @@
+import { fetchMatchDetail } from "@/apis/matchApis";
 import MatchDetailTemplate from "@/components/templates/MatchDetailTemplate";
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
 
 const MatchDetailPage = () => {
+  const { match_id } = useParams<{ match_id: string }>();
+  // MatchDatail get api 호출
+  const { data, isLoading } = useQuery({
+    queryKey: ["matchDetail", match_id],
+    queryFn: async () => {
+      const response = await fetchMatchDetail(Number(match_id));
+      return response;
+    },
+  });
   return (
     <div>
-      <MatchDetailTemplate />
+      {!isLoading && data && (
+        <MatchDetailTemplate matchDetailPropsData={data} />
+      )}
     </div>
   );
 };
