500afaf80601914a59372208fdc09c2afb6a5801
choiseongho <chltjdgh3@naver.com>
2024-03-28 18:04:05 +0900

    Feat: prop 존재하면 업로드 추가

--- frontend/mancity-project/src/components/organisms/profile_userStats/ProfileUserStats.tsx
+++ frontend/mancity-project/src/components/organisms/profile_userStats/ProfileUserStats.tsx
@@ -14,24 +14,28 @@ const ProfileUserStats = ({ profileData }: ProfilePropsType) => {
     }
   }, [data]);
   return (
-    <div id="glassui" className="justify-center m-4">
-      <div className=" p-4">
-        <div className="-mr-4 ">
-          <Typography
-            label="개인 스텟"
-            textColor="black"
-            textSize="text-2xl"
-            fontWeight="font-medium"
-          />
-          <hr className="border-[#d9d9d9] border mt-3 w-[96%]" />
-          <div className="m-4">
-            <RadarChart profileData={profileData} />
+    <div>
+      {profileData && (
+        <div id="glassui" className="justify-center m-4">
+          <div className=" p-4">
+            <div className="-mr-4 ">
+              <Typography
+                label="개인 스텟"
+                textColor="black"
+                textSize="text-2xl"
+                fontWeight="font-medium"
+              />
+              <hr className="border-[#d9d9d9] border mt-3 w-[96%]" />
+              <div className="m-4">
+                <RadarChart profileData={profileData} />
+              </div>
+            </div>
+            <div className="flex justify-end -my-2 underline decoration-white	 mr-3 pb-2 cursor-pointer">
+              <MuiModal userId={profileData.id} />
+            </div>
           </div>
         </div>
-        <div className="flex justify-end -my-2 underline decoration-white	 mr-3 pb-2 cursor-pointer">
-          <MuiModal userId={profileData.id} />
-        </div>
-      </div>
+      )}
     </div>
   );
 };
