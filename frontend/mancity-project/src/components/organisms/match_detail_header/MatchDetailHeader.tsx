16947aa4ad12001e0cb510e80e9156d3161c0bbc
choiseongho <chltjdgh3@naver.com>
2024-03-29 15:53:13 +0900

    Feat: 경기 상세페이지 정보 prop 으로 파싱

--- frontend/mancity-project/src/components/organisms/match_detail_header/MatchDetailHeader.tsx
+++ frontend/mancity-project/src/components/organisms/match_detail_header/MatchDetailHeader.tsx
@@ -1,19 +1,41 @@
 import Typography from "@/components/atoms/typography/Typography";
 
-const MatchDetailHeader = () => {
+// 한국 날짜 설정
+import dayjs from "dayjs";
+import "dayjs/locale/ko";
+dayjs.locale("ko");
+
+const MatchDetailHeader = ({
+  matchDetailPropsData,
+  courtData,
+}: MatchDetailPropsType) => {
+  // 날짜 형식 변경
+  const date = dayjs(matchDetailPropsData.startDate).format("M월 D일 (ddd)");
+
+  // 받은 시간을 오전, 오후 확인 후 형식 변경
+  const time = matchDetailPropsData.time;
+  const formatTime = (time: number) => {
+    const isAM = time < 12;
+    let formattedHour = time % 12;
+    if (formattedHour === 0) {
+      formattedHour = 12; // 0시 혹은 12시는 12로 표시
+    }
+    return isAM ? `오전 ${formattedHour}시` : `오후 ${formattedHour}시`;
+  };
+
   return (
     <div id="glassui" className="flex flex-col justify-between p-3 m-3 ">
       <div className="flex justify-between px-2">
         <div>
           <div>
             <Typography
-              label="광주 신화 풋살장"
+              label={courtData?.title}
               fontWeight="font-semibold"
               textSize="text-[1.6rem]"
             />
           </div>
           <Typography
-            label="3월 12일 (화) 오전 10시"
+            label={`${date} ${formatTime(time)}`}
             fontWeight="font-medium"
             textSize="text-base"
           />
@@ -31,7 +53,7 @@ const MatchDetailHeader = () => {
         </div>
         <div>
           <Typography
-            label="하남최성호"
+            label={matchDetailPropsData.manager}
             fontWeight="font-medium"
             textSize="text-sm"
             textColor="text-gray-500"
--- frontend/mancity-project/src/components/templates/MatchDetailTemplate.tsx
+++ frontend/mancity-project/src/components/templates/MatchDetailTemplate.tsx
@@ -3,6 +3,7 @@ import Header from "@/components/organisms/header/Header";
 import MatchDetailBody from "@/components/organisms/match_detail_body/MatchDetailBody";
 import MatchDetailHeader from "@/components/organisms/match_detail_header/MatchDetailHeader";
 import { useNavigate } from "react-router-dom";
+import futsalCourtData from "@/data/futsalCourts.json";
 
 const MatchDetailTemplate = ({
   matchDetailPropsData,
@@ -14,20 +15,39 @@ const MatchDetailTemplate = ({
   const onButtonClick = () => {
     navigate("/");
   };
+
+  const courtData = futsalCourtData.find(
+    (court) => court.id === matchDetailPropsData.courtId
+  );
+
+  const propData = matchDetailPropsData;
+  const gender = `${propData?.gender === 1 ? "남자" : propData?.gender === 2 ? "여자" : "혼성"}`;
+  const playerNumber = `${propData?.playerNumber === 10 ? "5vs5" : "6vs6"}`;
+
   return (
     <div>
-      <Header label="매치 상세" backArrow={true} headerButton={false} />
-      <MatchDetailHeader />
-      <MatchDetailBody />
+      {courtData && (
+        <div>
+          <Header label="매치 상세" backArrow={true} headerButton={false} />
+          <MatchDetailHeader
+            matchDetailPropsData={matchDetailPropsData}
+            courtData={courtData}
+          />
+          <MatchDetailBody
+            matchDetailPropsData={matchDetailPropsData}
+            courtData={courtData}
+          />
 
-      {/* Footer의 크기만큼 스크롤이 되지 않아서 공백 삽입 */}
-      <div className="h-20"></div>
+          {/* Footer의 크기만큼 스크롤이 되지 않아서 공백 삽입 */}
+          <div className="h-20"></div>
 
-      <Footer
-        label="남자 ∙ 5vs5 ∙ 세미프로"
-        buttonLabel="매치 참여 신청"
-        onButtonClick={onButtonClick}
-      />
+          <Footer
+            label={`${gender} ∙ ${playerNumber} ∙ ${propData.level} `}
+            buttonLabel="매치 참여 신청"
+            onButtonClick={onButtonClick}
+          />
+        </div>
+      )}
     </div>
   );
 };
