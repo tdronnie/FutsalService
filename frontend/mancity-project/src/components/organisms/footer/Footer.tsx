5442e3da5e424bf357b126313285126b005388ca
choiseongho <chltjdgh3@naver.com>
2024-03-30 10:24:20 +0900

    Docs: 사용하지 않는 콘솔로그 삭제

--- frontend/mancity-project/src/components/organisms/footer/Footer.tsx
+++ frontend/mancity-project/src/components/organisms/footer/Footer.tsx
@@ -4,7 +4,10 @@ import MyTypography from "@/components/atoms/my_typography/MyTypography";
 const Footer = (props: FooterPropsType) => {
   const { label, buttonLabel, onButtonClick } = props;
   return (
-    <div id="glassnav" className="rounded-t-xl fixed bottom-0 z-10 bg-white w-full max-w-[36rem]">
+    <div
+      id="glassnav"
+      className="rounded-t-xl fixed bottom-0 z-10 bg-white w-full max-w-[36rem]"
+    >
       <div className="flex items-center justify-around h-16 border-t-2 rounded-t-xl">
         <div className="w-1/2 text-center">
           <MyTypography
