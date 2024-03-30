c952bba30e6ff2bce75c1a14461bb5ac208aa253
SE EUN LEE <senyvee9@gmail.com>
2024-03-30 21:31:45 +0900

    Fix: 경기 하이라이트 조회 로직 수정

--- social/src/main/java/com/mancity/social/highlight/presentation/HighlightController.java
+++ social/src/main/java/com/mancity/social/highlight/presentation/HighlightController.java
@@ -9,17 +9,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/api/social/highlight")
 public class HighlightController {
 
     private final HighlightService highlightService;
 
 
-    @GetMapping("/highlights{game}")
+    @GetMapping("/{game}")
     @Operation(summary = "하이라이트 조회 API", description = "경기 분석 후 생성된 하이라이트 조회 API")
     public ResponseEntity<List<HighlightReponseDto>> getGameHighlights(@PathVariable("game") Long id) {
         return new ResponseEntity<>(highlightService.getGameHighlights(id), HttpStatus.OK);
