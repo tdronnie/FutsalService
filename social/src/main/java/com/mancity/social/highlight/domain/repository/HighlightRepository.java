9b113dc085064a10be9e9c7354e0f671a0172c17
이세은 <senyvee9@gmail.com>
2024-04-01 13:46:34 +0900

    Fix: HighlighthServvice myhighlight 저장 로직 변경 및 socialFeign 삭제

--- social/src/main/java/com/mancity/social/highlight/domain/repository/HighlightRepository.java
+++ social/src/main/java/com/mancity/social/highlight/domain/repository/HighlightRepository.java
@@ -3,7 +3,11 @@ package com.mancity.social.highlight.domain.repository;
 import com.mancity.social.highlight.domain.Highlight;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface HighlightRepository extends JpaRepository<Highlight, Long> {
+
+    public List<Highlight> findByGameId(Long id);
 
 
 }
