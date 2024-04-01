88fe1357110b4e90a61a445ad7756f3e38cc76dc
choiseongho <chltjdgh3@naver.com>
2024-04-01 09:56:03 +0900

    Remove: 가로화면에 맞추어 제작했던 전술보드 제거

--- frontend/mancity-project/src/stores/tacticalBoardStore.ts
+++ frontend/mancity-project/src/stores/tacticalBoardStore.ts
@@ -1,156 +1,83 @@
 import create from "zustand";
 
 interface TacticalBoardStoreType {
-  horizonPlayers: {
-    x: number;
-    y: number;
-  }[];
-  verticalPlayers: {
+  players: {
     x: number;
     y: number;
   }[];
 
-  setHorizonPosition: (index: number, x: number, y: number) => void;
-  setVerticalPosition: (index: number, x: number, y: number) => void;
+  setPosition: (index: number, x: number, y: number) => void;
 }
 
-// 브라우저의 너비, 높이
-const screenWidth = window.innerWidth;
-const screenHeight = window.innerHeight;
-
-// 가로화면 플레이어 default 위치
-const horizonPlayers = [
-  // A team
-  {
-    x: screenWidth < 577 ? screenWidth * 0.27 : 576 * 0.27,
-    y: screenHeight * 0.25,
-  },
-  {
-    x: screenWidth < 577 ? screenWidth * 0.65 : 576 * 0.65,
-    y: screenHeight * 0.25,
-  },
-  {
-    x: screenWidth < 577 ? screenWidth * 0.1 : 576 * 0.1,
-    y: screenHeight * 0.35,
-  },
-  {
-    x: screenWidth < 577 ? screenWidth * 0.48 : 576 * 0.48,
-    y: screenHeight * 0.35,
-  },
-  {
-    x: screenWidth < 577 ? screenWidth * 0.8 : 576 * 0.8,
-    y: screenHeight * 0.35,
-  },
-  {
-    x: screenWidth < 577 ? screenWidth * 0.48 : 576 * 0.48,
-    y: screenHeight * 0.11,
-  },
-  // B team
-  {
-    x: screenWidth < 577 ? screenWidth * 0.25 : 576 * 0.25,
-    y: screenHeight * 0.65,
-  },
-  {
-    x: screenWidth < 577 ? screenWidth * 0.65 : 576 * 0.65,
-    y: screenHeight * 0.65,
-  },
-  {
-    x: screenWidth < 577 ? screenWidth * 0.1 : 576 * 0.1,
-    y: screenHeight * 0.55,
-  },
-  {
-    x: screenWidth < 577 ? screenWidth * 0.48 : 576 * 0.48,
-    y: screenHeight * 0.55,
-  },
-  {
-    x: screenWidth < 577 ? screenWidth * 0.8 : 576 * 0.8,
-    y: screenHeight * 0.55,
-  },
-  {
-    x: screenWidth < 577 ? screenWidth * 0.48 : 576 * 0.48,
-    y: screenHeight * 0.777,
-  },
-  // ball
-  {
-    x: screenWidth < 577 ? screenWidth * 0.48 : 576 * 0.48,
-    y: screenHeight * 0.44,
-  },
-];
+// 브라우저의 너비, 높이의 절반
+const screenWidth = window.innerWidth / 2 - 15;
+// const screenHeight = window.innerHeight ;
 
 // 세로화면 플레이어 default 위치
-const verticalPlayers = [
+const players = [
   // A team
   {
-    x: screenWidth < 577 ? screenWidth * 0.28 : 576 * 0.28,
-    y: screenWidth < 577 ? screenWidth * 0.2 : 576 * 0.2,
+    x: screenWidth * 2 < 577 ? screenWidth - 80 : 195,
+    y: 140,
   },
   {
-    x: screenWidth < 577 ? screenWidth * 0.28 : 576 * 0.28,
-    y: screenWidth < 577 ? screenWidth * 0.4 : 576 * 0.4,
+    x: screenWidth * 2 < 577 ? screenWidth + 80 : 350,
+    y: 140,
   },
   {
-    x: screenWidth < 577 ? screenWidth * 0.38 : 576 * 0.38,
-    y: screenWidth < 577 ? screenWidth * 0.1 : 576 * 0.1,
+    x: screenWidth * 2 < 577 ? screenWidth - 125 : 150,
+    y: 220,
   },
   {
-    x: screenWidth < 577 ? screenWidth * 0.38 : 576 * 0.38,
-    y: screenWidth < 577 ? screenWidth * 0.31 : 576 * 0.31,
+    x: screenWidth * 2 < 577 ? screenWidth : 275,
+    y: 220,
   },
   {
-    x: screenWidth < 577 ? screenWidth * 0.38 : 576 * 0.38,
-    y: screenWidth < 577 ? screenWidth * 0.5 : 576 * 0.5,
+    x: screenWidth * 2 < 577 ? screenWidth + 125 : 400,
+    y: 220,
   },
   {
-    x: screenWidth < 577 ? screenWidth * 0.11 : 576 * 0.11,
-    y: screenWidth < 577 ? screenWidth * 0.31 : 576 * 0.31,
+    x: screenWidth * 2 < 577 ? screenWidth : 275,
+    y: 60,
   },
-  // B team
+  // // B team
   {
-    x: screenWidth < 577 ? screenWidth * 0.67 : 576 * 0.67,
-    y: screenWidth < 577 ? screenWidth * 0.2 : 576 * 0.2,
-  },
-  {
-    x: screenWidth < 577 ? screenWidth * 0.67 : 576 * 0.67,
-    y: screenWidth < 577 ? screenWidth * 0.4 : 576 * 0.4,
+    x: screenWidth * 2 < 577 ? screenWidth - 80 : 195,
+    y: 380,
   },
   {
-    x: screenWidth < 577 ? screenWidth * 0.57 : 576 * 0.57,
-    y: screenWidth < 577 ? screenWidth * 0.1 : 576 * 0.1,
+    x: screenWidth * 2 < 577 ? screenWidth + 80 : 350,
+    y: 380,
   },
   {
-    x: screenWidth < 577 ? screenWidth * 0.57 : 576 * 0.57,
-    y: screenWidth < 577 ? screenWidth * 0.31 : 576 * 0.31,
+    x: screenWidth * 2 < 577 ? screenWidth - 125 : 150,
+    y: 300,
   },
   {
-    x: screenWidth < 577 ? screenWidth * 0.57 : 576 * 0.57,
-    y: screenWidth < 577 ? screenWidth * 0.5 : 576 * 0.5,
+    x: screenWidth * 2 < 577 ? screenWidth : 275,
+    y: 300,
   },
   {
-    x: screenWidth < 577 ? screenWidth * 0.84 : 576 * 0.84,
-    y: screenWidth < 577 ? screenWidth * 0.31 : 576 * 0.31,
+    x: screenWidth * 2 < 577 ? screenWidth + 125 : 400,
+    y: 300,
   },
-  // ball
   {
-    x: screenWidth < 577 ? screenWidth * 0.48 : 576 * 0.48,
-    y: screenWidth < 577 ? screenWidth * 0.31 : 576 * 0.31,
+    x: screenWidth * 2 < 577 ? screenWidth : 275,
+    y: 450,
+  },
+  // // ball
+  {
+    x: screenWidth * 2 < 577 ? screenWidth : 275,
+    y: 258,
   },
 ];
 
 // Zustand 스토어 생성 및 타입 적용
 const TacticalBoardStore = create<TacticalBoardStoreType>((set) => ({
-  horizonPlayers: horizonPlayers,
-  verticalPlayers: verticalPlayers,
-  setHorizonPosition: (index, x, y) =>
+  players: players,
+  setPosition: (index, x, y) =>
     set((state) => ({
-      horizonPlayers: state.horizonPlayers.map((p, i) =>
-        i === index ? { ...p, x, y } : p
-      ),
-    })),
-  setVerticalPosition: (index, x, y) =>
-    set((state) => ({
-      verticalPlayers: state.verticalPlayers.map((p, i) =>
-        i === index ? { ...p, x, y } : p
-      ),
+      players: state.players.map((p, i) => (i === index ? { ...p, x, y } : p)),
     })),
 }));
 
