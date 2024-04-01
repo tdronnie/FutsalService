f52f0278c21297b8ee5b2b02389d5bfb19ef542b
choiseongho <chltjdgh3@naver.com>
2024-04-02 02:18:01 +0900

    Feat: 경기 상세페이지 store 생성

--- frontend/mancity-project/src/stores/matchStore.ts
+++ frontend/mancity-project/src/stores/matchStore.ts
@@ -1,3 +1,18 @@
-export const matchStore = () => {
-  return;
-};
+import create from "zustand";
+
+interface UploadState {
+  isUpload: boolean;
+  changeIsUpload: () => void;
+  isStartUpload: boolean;
+  changeIsStartUpload: () => void;
+}
+
+const useIsUploadStore = create<UploadState>((set) => ({
+  isUpload: false,
+  isStartUpload: false,
+  changeIsUpload: () => set((state) => ({ isUpload: !state.isUpload })),
+  changeIsStartUpload: () =>
+    set((state) => ({ isStartUpload: !state.isStartUpload })),
+}));
+
+export default useIsUploadStore;
