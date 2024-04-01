fee82cf6b4f396c2b38a67a203aecb7427bf2907
choiseongho <chltjdgh3@naver.com>
2024-04-01 09:57:59 +0900

    Feat: 다시보기 페이지에 전술보드 모달 추가

--- frontend/mancity-project/src/components/templates/analysis/ReplayTemplate.tsx
+++ frontend/mancity-project/src/components/templates/analysis/ReplayTemplate.tsx
@@ -1,8 +1,10 @@
+import Header from "@/components/organisms/header/Header";
 import ReplayBody from "@/components/organisms/replay_body/ReplayBody";
 
 const ReplayTemplate = () => {
   return (
     <div>
+      <Header backArrow={true} label="경기 다시보기" headerButton={false} />
       <ReplayBody />
     </div>
   );
