9bd7b0acd7359ec9a6d87eec3dcbbdca9608df35
Jiyong <aop8021@naver.com>
2024-04-02 19:00:26 +0900

    Feat: 클럽생성 api 생성

--- frontend/mancity-project/src/apis/clubApis.ts
+++ frontend/mancity-project/src/apis/clubApis.ts
@@ -26,3 +26,16 @@ export const fetchplayersApi = async () => {
     throw new Error("용병리스트 정보 api 에러");
   }
 };
+
+// POST 요청 API
+// 클럽 생성
+export const createClubApi = async (clubData: FormData) => {
+  return publicRequest
+    .post(`user/club/create`, clubData, {
+    })
+    .then((res) => res.data)
+    .catch((error) => {
+      console.log(error);
+      throw new Error("클럽 생성 api 에러");
+    });
+};
