8a19be616dfd2f793bd5205cd9695ec2ee139504
예준성 <306yyy@naver.com>
2024-04-02 23:08:42 +0900

    Fix: 찾았다 Former-commit-id: b91b287ecc811594db5360b5e8a9e0d95479eced

--- social/src/main/java/com/mancity/social/flask/presentation/FlaskFeignClient.java
+++ social/src/main/java/com/mancity/social/flask/presentation/FlaskFeignClient.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(value = "flask", url = "http://70.12.130.121:8090")
+// @FeignClient(value = "flask", url = "http://70.12.130.121:8090")
+@FeignClient(value = "flask", url = "http://175.209.203.185:8090")
 public interface FlaskFeignClient {
 
     @PostMapping("/api/flask/track")
