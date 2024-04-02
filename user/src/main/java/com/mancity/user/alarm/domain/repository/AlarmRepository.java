2aa7cae637d4db60ff7972c975b77a7de43322d8
예준성 <306yyy@naver.com>
2024-04-02 13:24:24 +0900

    Feat: 알람 id 내림차순 정렬
    Former-commit-id: 70e2f76c2dc9959951e100055c1b278bd7c4aca2

--- user/src/main/java/com/mancity/user/alarm/domain/repository/AlarmRepository.java
+++ user/src/main/java/com/mancity/user/alarm/domain/repository/AlarmRepository.java
@@ -2,11 +2,13 @@ package com.mancity.user.alarm.domain.repository;
 
 import com.mancity.user.alarm.domain.Alarm;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Collection;
 import java.util.List;
 
 public interface AlarmRepository extends JpaRepository<Alarm, Long> {
-    List<Alarm> findAllByReceiverId(long receiverId);
+//    @Query("select a from Alarm a where a.receiverId = :receiverId order by f.id desc")
+    List<Alarm> findAllByReceiverIdOrderByIdDesc(long receiverId);
 
 }
