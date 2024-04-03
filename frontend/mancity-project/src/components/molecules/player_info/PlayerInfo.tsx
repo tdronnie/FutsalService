64e088e5d367eb9dcc043fb0d46f9fa02beb16ee
choiseongho <chltjdgh3@naver.com>
2024-04-04 05:48:18 +0900

    Feat: 데이터 단위 수정

--- frontend/mancity-project/src/components/molecules/player_info/PlayerInfo.tsx
+++ frontend/mancity-project/src/components/molecules/player_info/PlayerInfo.tsx
@@ -173,8 +173,8 @@ const PlayerInfo = (props: PlayerInfoPropsType) => {
                         fontWeight="font-medium"
                         label={
                           playerData
-                            ? `: ${playerData.distanceCovered}km`
-                            : ": 1.2km"
+                            ? `: ${playerData.distanceCovered}m`
+                            : ": 1200m"
                         }
                         textColor="text-darkcity"
                         textSize="text-xl"
