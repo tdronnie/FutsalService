ae63f8e3de18f2ae295dd0db5f7bff5d38fa3508
choiseongho <chltjdgh3@naver.com>
2024-04-04 13:06:49 +0900

    delelete files
    Former-commit-id: 324e83a28883089da2f0179f396c06fe5922d54e

--- calc/src/main/java/com/mancity/calc/gamedata/domain/respository/GamedataRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mancity.calc.gamedata.domain.respository;
-
-
-import com.mancity.calc.gamedata.domain.Data;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GamedataRepository extends JpaRepository<Data, Long> {
-}
