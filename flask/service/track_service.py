f34494a89e3348c0a944251467f961a4726507cd
윤지현 <goon03077@gmail.com>
2024-04-04 11:46:11 +0900

    Feat: flask 서버 구현
    Former-commit-id: cdac42cce661ea813c40f911803e6d1919ca9fb9

--- flask/service/track_service.py
+++ flask/service/track_service.py
@@ -1,121 +1,113 @@
-from service.tracking import track
-import json
-
-team_A_players = []
-team_B_players = []
-
-goal_post_A_id = {}
-goal_post_B_id = {}
-
-id_map = {}
-
-field = {
-    'x1':0,
-    'x2':1920,
-    'y1':0,
-    'y2':1080
-}
-
-def get_results(source, game_id):
-    results = track(source)
+from service.tracking.model import model
+from service import util
+import requests
+import os
+import cv2
+import time
 
 
-def player_field_filter(players):
-    x_padding = 500
-    y_padding = 300
-    filtered_players = []
+class track_service:
+    def __init__(self):
+        self.source = None
+        self.team_A_players = []
+        self.team_B_players = []
+        self.team_A_goal_post = {}
+        self.team_B_goal_post = {}
+        self.ball = {}
+        self.field = {
+            'x1': 0,
+            'x2': 1920,
+            'y1': 0,
+            'y2': 1080
+        }
+        self.team_A_player_id_map = {}
+        self.team_B_player_id_map = {}
+        self.lost_players = []
+        self.model = model()
 
-    for player in players:
-        position_x = player.get('position')[0]
-        position_y = player.get('position')[1]
-        if position_x < field.get('x1') - x_padding or position_x > field.get(
-                'x2') + x_padding or position_y < field.get('y1') - y_padding or position_y > field.get(
-                'y2') + y_padding:
-            continue
-        filtered_players.append(player)
-    return filtered_players
+    def get_result(self, source):
+        self.source = source
 
+        save_path = './service/video/'
+        filename = source.split('/')[-1]
 
-def player_team_divider(players):
-    players.sort(key=lambda x: x['c_score'])
-    total_player_cnt = len(players)
-    half = None
-    team_A_players = []
-    team_B_players = []
+        start = time.time()
+        with requests.get(source, stream=True) as r:
+            p = 0
+            with open(save_path + filename, 'wb') as f:
+                for chunk in r.iter_content(chunk_size=100 * 1024 * 1024):
+                    p += 1
+                    print(p)
+                    f.write(chunk)
+        end = time.time()
+        print(end - start)
 
-    if total_player_cnt < 10:
-        return None
-    elif total_player_cnt == 10 or total_player_cnt == 11:
-        half = 5
-    else:
-        half = 6
+        result = {'data': []}
+        cap = cv2.VideoCapture(self.source)
+        frame_num = 0
+        last_ball = {}
+        defined = False
+        defined_frame = 0
 
-    players.sort(key=lambda x: x['position'][0])
-    team_A_players = players[:half]
-    team_B_players = players[half:]
-    return (team_A_players, team_B_players)
+        while cap.isOpened():
+            success, frame = cap.read()
+            if success:
+                frame_num += 1
+                track_result = eval(self.model.track(frame)[0].tojson())
 
+                objs_field, objs_goal_post, objs_player = util.obj_divider(track_result)
 
-def goal_post_team_divider(goal_posts):
-    goal_posts.sort(key=lambda x: x['c_score'])
-    goal_posts = goal_posts[:2]
-    goal_posts.sort(key=lambda x: x['x1'])
-    return goal_posts[0], goal_posts[1]
+                self.field = util.validator_field(self.field, objs_field)
 
+                detect_result = eval(self.model.detect(frame)[0].tojson())
 
-# 리턴 객체에 포함될 데이터
-# team_A_goal_post
-# team_B_goal_post
-# team_A_players
-# team_B_players
+                if detect_result is None:
+                    self.ball = None
+                else:
+                    self.ball = util.validator_ball(frame_num, last_ball, detect_result)
+                    if self.ball is not None:
+                        last_ball = {
+                            'frame_num': frame_num,
+                            'x': self.ball.get('x'),
+                            'y': self.ball.get('y')
+                        }
+                if not defined:
+                    result_info = util.define_objs(self.field, objs_goal_post, objs_player)
+                    self.team_A_players = result_info.get('team_A_players')
+                    self.team_B_players = result_info.get('team_B_players')
+                    self.team_A_goal_post = result_info.get('team_A_goal_post')
+                    self.team_B_goal_post = result_info.get('team_B_goal_post')
+                    self.team_A_player_id_map = result_info.get('team_A_player_id_map')
+                    self.team_B_player_id_map = result_info.get('team_B_player_id_map')
+                    if not None in result_info.values():
+                        defined = True
+                        defined_frame = frame_num - 1
+                        temp = self.model.track(frame)[0]
+                        temp.plot(show=True, conf=False, font_size=3, labels=True)
+                        temp.show()
+                        temp.save(filename='test.jpg')
+                        util.write_defined_image(frame, filename, result_info)
+                else:
+                    if util.validator_goal_post(objs_goal_post) is not None:
+                        self.team_A_goal_post, self.team_B_goal_post = util.validator_goal_post(objs_goal_post)
 
-def define_objects(objs):
-    #     if len(objs)<16:
-    #         return -1
-    players = []
-    goal_posts = []
-    balls = []
+                    self.team_A_players, self.team_B_players, self.team_A_player_id_map, self.team_B_player_id_map, self.lost_players = \
+                        util.validator_player(self.team_A_player_id_map, self.team_B_player_id_map, self.lost_players,
+                                              objs_player)
 
-    for obj in objs:
-        obj_name = obj.get('name')
-        if obj_name == 'player':
-            players.append({
-                'id': obj.get('track_id'),
-                'position': get_position(obj.get('box')),
-                'c_score': obj.get('confidence')
-            })
-        elif obj_name == 'goal_post':
-            box = obj.get('box')
-            goal_posts.append({
-                'id': obj.get('track_id'),
-                'x1': box.get('x1'),
-                'x2': box.get('x2'),
-                'y1': box.get('y1'),
-                'y2': box.get('y2'),
-                'c_score': obj.get('confidence')
-            })
-
-    # 탐지된 player와 goal_post의 confidence score 순서로 객체 갯수 만큼 자르기
-    # player : 경기장 밖 객체는 제외
-    players = player_field_filter(players)
-
-    # confidence score 순서로 10명 또는 12명으로 제한 (total_player_count/2)
-    # position x 위치 순서로 왼쪽 선수들은 A팀, 오른쪽 선수들은 B팀
-    team_A_players, team_B_players = player_team_divider(players)
-    team_A_goal_post, team_B_goal_post = goal_post_team_divider(goal_posts)
-
-    return {
-        'team_A_players': team_A_players,
-        'team_B_players': team_B_players,
-        'team_A_goal_post': team_A_goal_post,
-        'team_B_goal_post': team_B_goal_post
-    }
-
-
-def get_position(box) -> float:
-    position = -1
-    position = (box.get('x1') + (box.get('x2')-box.get('x1')) , box.get('y1')+(box.get('y2')-box.get('y1')))
-    return position
-
-def get_mapped_id(id_map, origin_id) -> int:
-    return id_map.get(str(origin_id))
+                result.get('data').append({
+                    'frame_num': frame_num,
+                    'field': util.change_int(self.field),
+                    'ball': self.ball,
+                    'team_A_goal_post': util.change_int(self.team_A_goal_post),
+                    'team_B_goal_post': util.change_int(self.team_B_goal_post),
+                    'team_A_players': util.set_format(self.team_A_player_id_map, self.team_A_players),
+                    'team_B_players': util.set_format(self.team_B_player_id_map, self.team_B_players)
+                })
+                print('frame_num', frame_num)
+            else:
+                cap.release()
+        print('len(result) : ', result)
+        result.update({'data': util.interpolation(defined_frame, result.get('data'))})
+        print("done")
+        return result
